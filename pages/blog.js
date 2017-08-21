4d6cf1a4c3eb8d971fe9b06704f27d46afdd8b85
Koen van Gilst <koen.vangilst@gmail.com>
2017-08-21 15:55:23 +0200

    adds blog, hides profile photo for now

---
+++ pages/blog.js
@@ -0,0 +1,16 @@
+import Layout from '../components/layout'
+
+export default () =>
+  <Layout>
+    <section className='creations'>
+      <div className='wrapper'>
+        <div className='project project--double'>
+          <h1>Coming soon...</h1>
+          <p>
+            Read my articles on
+            <a href='https://medium.com/@vnglst/latest'> Medium</a> until then.
+          </p>
+        </div>
+      </div>
+    </section>
+  </Layout>
