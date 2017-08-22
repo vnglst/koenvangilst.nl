f71d849a34dc06250faf9e36da979500408d4774
Koen van Gilst <koen.vangilst@gmail.com>
2017-08-22 14:52:09 +0200

    moves css to styled jsx, remove normalize.css

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,14 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Analytics from './analytics'
+import Footer from './footer'
+import { global } from './styles'
 
 export default ({ children, title = 'Koen van Gilst', menu = 'home' }) =>
   <div>
+    <style jsx global>
+      {global}
+    </style>
     <Head>
       <meta charset='utf-8' />
       <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1' />
@@ -15,13 +20,11 @@ export default ({ children, title = 'Koen van Gilst', menu = 'home' }) =>
         content='LRaILkORB_X4mP7mG-Qfct597xXWzzzqfMG0XK6LXWI'
       />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <link rel='stylesheet' href='/static/css/normalize.css' />
       {/* <link rel="stylesheet" href="/static/css/font-awesome.min.css" /> */}
       <link
         rel='stylesheet'
         href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
       />
-      <link rel='stylesheet' href='/static/css/main.css' />
       <link
         rel='shortcut icon'
         type='image/png'
@@ -58,51 +61,6 @@ export default ({ children, title = 'Koen van Gilst', menu = 'home' }) =>
       {children}
     </div>
 
-    <footer>
-      <div className='footer-content'>
-        <span className='social-links'>
-          <a
-            target='_blank'
-            href='https://twitter.com/vnglst'
-            title='Follow me on Twitter'
-          >
-            <i
-              className='fa fa-lg fa-twitter'
-              style={{ color: '#4099FF' }}
-            />Twitter
-          </a>
-          <a
-            target='_blank'
-            href='https://github.com/vnglst'
-            title='View code samples on GitHub'
-          >
-            <i
-              className='fa fa-lg fa-github'
-              style={{ color: '#36382E' }}
-            />GitHub
-          </a>
-          <a
-            target='_blank'
-            href='https://www.linkedin.com/in/vangilst/'
-            title='View my resume on LinkedIn'
-          >
-            <i
-              className='fa fa-lg fa-linkedin'
-              style={{ color: '#007BB6' }}
-            />LinkedIn
-          </a>
-        </span>
-        <span className='attribution'>
-          Design inspired by{' '}
-          <a
-            target='_blank'
-            href='http://tholman.com/'
-            title='Awesome Tim Holman'
-          >
-            Tim Holman
-          </a>
-        </span>
-      </div>
-    </footer>
+    <Footer />
     <Analytics />
   </div>
