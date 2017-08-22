c0bfe83c000b6eddabceabf96b55958ca1bb87eb
Koen van Gilst <koen.vangilst@gmail.com>
2017-08-22 08:42:38 +0200

    use preact instead of react

--- next.config.js
+++ next.config.js
@@ -6,5 +6,19 @@ module.exports = {
       '/profile': { page: '/profile' },
       '/dedicon': { page: '/dedicon' }
     }
+  },
+  webpack: function (config, { dev }) {
+    // For the development version, we'll use React.
+    // Because, it supports react hot loading and so on.
+    if (dev) {
+      return config
+    }
+
+    config.resolve.alias = {
+      'react': 'preact-compat/dist/preact-compat',
+      'react-dom': 'preact-compat/dist/preact-compat'
+    }
+
+    return config
   }
 }
