31ce92e8e2312ca323b304f05696181699155df7
Koen van Gilst <koen.vangilst@gmail.com>
2018-07-19 19:58:41 +0200

    remove Google analytics tracking shit

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,11 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Analytics from './analytics'
 import Footer from './footer'
 import { global } from './styles'
 import { normalize } from './normalize'
 import { fontAwesome } from './font-awesome'
 
-export default ({ children, title = "Koen van Gilst", menu = "home" }) =>
+export default ({ children, title = 'Koen van Gilst', menu = 'home' }) => (
   <div>
     <style jsx global>
       {normalize}
@@ -20,13 +19,7 @@ export default ({ children, title = "Koen van Gilst", menu = "home" }) =>
     <Head>
       <meta charset="utf-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-      <title>
-        {title}
-      </title>
-      <meta
-        name="google-site-verification"
-        content="LRaILkORB_X4mP7mG-Qfct597xXWzzzqfMG0XK6LXWI"
-      />
+      <title>{title}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link
         rel="shortcut icon"
@@ -47,23 +40,21 @@ export default ({ children, title = "Koen van Gilst", menu = "home" }) =>
           </span>
           <span className="nav-links">
             <Link prefetch href="/">
-              <a className={menu === "home" ? "active" : null}>Home</a>
+              <a className={menu === 'home' ? 'active' : null}>Home</a>
             </Link>
             <Link prefetch href="/profile">
-              <a className={menu === "profile" ? "active" : null}>Profile</a>
+              <a className={menu === 'profile' ? 'active' : null}>Profile</a>
             </Link>
             <Link prefetch href="/blog">
-              <a className={menu === "blog" ? "active" : null}>Blog</a>
+              <a className={menu === 'blog' ? 'active' : null}>Blog</a>
             </Link>
           </span>
         </div>
       </nav>
     </header>
 
-    <div className="content">
-      {children}
-    </div>
+    <div className="content">{children}</div>
 
     <Footer />
-    <Analytics />
   </div>
+)
