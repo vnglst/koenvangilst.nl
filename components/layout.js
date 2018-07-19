b8f2a085326ff5bc5de46b159caf2862cbee4303
Koen van Gilst <koen.vangilst@gmail.com>
2018-07-19 21:28:59 +0200

    adds some meta html head tags

--- components/layout.js
+++ components/layout.js
@@ -20,7 +20,12 @@ export default ({ children, title = 'Koen van Gilst', menu = 'home' }) => (
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
       <title>{title}</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="theme-color" content="#ff6600" />
+      <meta name="apple-mobile-web-app-title" content="koenvangilst.nl" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="mobile-web-app-capable" content="yes" />
       <link
         rel="shortcut icon"
         type="image/png"
