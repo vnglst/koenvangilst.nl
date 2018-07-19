aeeaa59abbb064ae7dfa428d9261a47b1b984988
Koen van Gilst <koen.vangilst@gmail.com>
2018-07-19 22:17:18 +0200

    removes preact, links not clickable on Safari iOS 😿

--- next.config.js
+++ next.config.js
@@ -1,24 +1,19 @@
 module.exports = {
-  exportPathMap: function () {
+  exportPathMap: function() {
     return {
       '/': { page: '/' },
       '/blog': { page: '/blog' },
       '/profile': { page: '/profile' },
-      '/dedicon': { page: '/dedicon' }
+      '/dedicon': { page: '/dedicon' },
     }
   },
-  webpack: function (config, { dev }) {
+  webpack: function(config, { dev }) {
     // For the development version, we'll use React.
     // Because, it supports react hot loading and so on.
     if (dev) {
       return config
     }
 
-    config.resolve.alias = {
-      'react': 'preact-compat/dist/preact-compat',
-      'react-dom': 'preact-compat/dist/preact-compat'
-    }
-
     return config
-  }
+  },
 }
