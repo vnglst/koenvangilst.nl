2035b3472266bf713adf15489e3c9b95c2e508f6
Koen van Gilst <koen.vangilst@gmail.com>
2018-07-21 13:06:52 +0200

    updates skills

--- pages/profile.js
+++ pages/profile.js
@@ -1,50 +1,58 @@
 import Layout from '../components/layout'
 
-export default () =>
-  <Layout title='Profile | Koen van Gilst' menu='profile' >
-    <section className='creations'>
-      <div className='wrapper'>
-        <div className='project'>
+export default () => (
+  <Layout title="Profile | Koen van Gilst" menu="profile">
+    <section className="creations">
+      <div className="wrapper">
+        <div className="project">
           <h1>Profile</h1>
           <p>
-            Entrepreneurial and enthousiastic JavaScript developer based in Utrecht (The Netherlands).
-            I specialize in Node and modern frontend using React.
-            I'm always looking for new challenging freelance work and I'm particularly interested in creating innovative web apps for inspiring companies.
+            Entrepreneurial and enthousiastic JavaScript developer based in
+            Utrecht (The Netherlands). I specialize in Node and modern frontend
+            using React. I'm always looking for new challenging freelance work
+            and I'm particularly interested in creating innovative web apps for
+            inspiring companies.
           </p>
           <p>
-            You can find me on <a href='https://github.com/vnglst'>Github</a>, <a href='http://nl.linkedin.com/in/vangilst/'>LinkedIn</a> and <a href='http://www.twitter.com/vnglst'>Twitter</a> or send me an{' '}
-            <a href='&#x6d;&#x61;&#x69;&#108;&#116;&#111;&#x3a;&#107;&#x6f;&#x65;&#x6e;&#x40;&#x6b;&#x6f;&#x65;&#x6e;&#x76;&#97;&#x6e;&#x67;&#x69;&#108;&#x73;&#116;&#x2e;&#x6e;&#x6c;'>
+            You can find me on <a href="https://github.com/vnglst">Github</a>,{' '}
+            <a href="http://nl.linkedin.com/in/vangilst/">LinkedIn</a> and{' '}
+            <a href="http://www.twitter.com/vnglst">Twitter</a> or send me an{' '}
+            <a href="&#x6d;&#x61;&#x69;&#108;&#116;&#111;&#x3a;&#107;&#x6f;&#x65;&#x6e;&#x40;&#x6b;&#x6f;&#x65;&#x6e;&#x76;&#97;&#x6e;&#x67;&#x69;&#108;&#x73;&#116;&#x2e;&#x6e;&#x6c;">
               email
             </a>.
           </p>
-
         </div>
-        <div className='project project--profile' />
+        <div className="project project--profile" />
 
-        <div className='project project--double'>
+        <div className="project project--double">
           <h1>Skills</h1>
           <ul>
             <li>
-              <b>Platforms:</b> Node.js, Browser (web)
+              <b>Platforms:</b> Web, Node.js, Mobile (iOS)
             </li>
             <li>
-              <b>Languages:</b> JavaScript (ES2015/16/17), HTML5, CSS3
+              <b>Languages:</b> JavaScript, TypeScript, HTML5, CSS3
             </li>
             <li>
-              <b>Frameworks:</b> Express, Next.js, React, Redux, jQuery
+              <b>Frameworks:</b> React, React Native, Redux, Express, Next.js,
+              jQuery, Redux, jQuery
             </li>
             <li>
-              <b>Tooling & Testing:</b> Chai, Mocha, Tape, Webpack, Babel, Jest,
-              Flow
+              <b>Tooling & Testing:</b> Webpack, Babel, Jest, Chai, Mocha,
+              Detox, Nock, Flow
             </li>
             <li>
-              <b>General:</b> MongoDB, JIRA, Git, Bash, Unix, PWAs, Web
-              Accessibility
+              <b>Knowledgeable:</b> Accessibility, Progressive Web Apps,
+              Internationalization, SEO
+            </li>
+            <li>
+              <b>Services:</b> Postgresql, MongoDB, Contentful, PhraseApp,
+              Bugsnag, BuddyBuild, Mixpanel, Heroku, AWS, Google Cloud, Jira
             </li>
           </ul>
         </div>
 
-        <div className='project' style={{ height: '170px' }}>
+        <div className="project" style={{ height: '170px' }}>
           <h1>Education</h1>
           <ul>
             <li>
@@ -59,10 +67,11 @@ export default () =>
           </ul>
         </div>
 
-        <div className='project' style={{ height: '170px' }}>
+        <div className="project" style={{ height: '170px' }}>
           <h1>References</h1>
           <p>References, code samples and a copy of my resume upon request.</p>
         </div>
       </div>
     </section>
   </Layout>
+)
