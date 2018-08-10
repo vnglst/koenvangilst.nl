2897ee688f4132ec31d1c7cb1c2bb89146b0d9b0
Koen van Gilst <koen.vangilst@gmail.com>
2018-08-10 11:34:58 +0200

    moving articles to blog tab

--- pages/index.js
+++ pages/index.js
@@ -111,36 +111,6 @@ export default () => (
           </span>
         </div>
         <div className="project">
-          <h1>API Testing with Jest</h1>
-          <p>
-            Jest is a great JavaScript testing framework by Facebook. It’s often
-            used for testing React components, but it’s also a pretty good
-            general purpose testing framework. Appeared in Hackernoon.
-          </p>
-          <span className="links">
-            <a href="https://hackernoon.com/api-testing-with-jest-d1ab74005c0a#.8yd4cfvvg">
-              Read it here
-            </a>{' '}
-            [<b>2017</b>]
-          </span>
-        </div>
-        <div className="project">
-          <h1>Generating PDF's with Node</h1>
-          <p>
-            A fun little Node tutorial I wrote on how to generate PDF's from a
-            web form using Express and Node. You can easily deploy it with Now.
-            Appeared in
-            <a href="http://nodeweekly.com/issues/163"> Node Weekly </a> on
-            November 10th.
-          </p>
-          <span className="links">
-            <a href="https://edgecoders.com/generating-a-pdf-with-express-in-node-js-d3ff5107dff1#.xrky44qvu">
-              Read it here
-            </a>{' '}
-            [<b>2016</b>].
-          </span>
-        </div>
-        <div className="project">
           <h1>Shortcut Trainer</h1>
           <p>
             Quickly learn the 10 most commonly used keyboard shortcuts for the
