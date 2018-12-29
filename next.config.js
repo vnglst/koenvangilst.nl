3463b10df54a45ce9eb68b61181dd67fb74403ba
Koen van Gilst <koen.vangilst@gmail.com>
2018-12-29 21:26:15 +0100

    remove unused dependency next mdx

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 const withCSS = require('@zeit/next-css')
-const withMDX = require('@zeit/next-mdx')()
 
-module.exports = withMDX(
-  withCSS({
-    exportPathMap: function() {
-      return {
-        '/': { page: '/' },
-        '/profile': { page: '/profile' },
-        '/dedicon': { page: '/dedicon' },
-        '/hilfiger': { page: '/hilfiger' }
-      }
+module.exports = withCSS({
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' },
+      '/profile': { page: '/profile' },
+      '/dedicon': { page: '/dedicon' },
+      '/hilfiger': { page: '/hilfiger' }
     }
-  })
-)
+  }
+})
