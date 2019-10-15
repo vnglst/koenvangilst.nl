4810f87078996051fc84d5183d95e5725e86e2cc
Koen van Gilst <vnglst@users.noreply.github.com>
2019-10-15 20:38:50 +0200

    chore() - fix link, updates (#32)

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Layout from '../components/layout'
-import Creations from '../components/creations'
-import Link from 'next/link'
+import Layout from "../components/layout";
+import Creations from "../components/creations";
+import Link from "next/link";
 
 export default () => (
   <Layout title="JavaScript Labs | Koen van Gilst" menu="home">
@@ -9,14 +9,14 @@ export default () => (
         <h1>Hi there!</h1>
         <p>
           My name is <b>Koen van Gilst</b> and I'm a JavaScript developer who
-          loves creating funny Twitter bots, educational games for my kids and other
-          creative stuff.
+          loves creating funny Twitter bots, educational games for my kids and
+          other creative stuff.
         </p>
         <p>
-          Have a look at my{' '}
-          <Link prefetch href="/profile">
+          Have a look at my{" "}
+          <Link href="/profile">
             <a>profile</a>
-          </Link>{' '}
+          </Link>{" "}
           if you're interested in working with me or else feel free to browse
           through my experiments and projects below.
         </p>
@@ -27,7 +27,7 @@ export default () => (
           You can also find me on
           <a href="https://github.com/vnglst"> Github</a>,
           <a href="https://nl.linkedin.com/in/vangilst/"> LinkedIn</a> and
-          <a href="https://www.twitter.com/vnglst"> Twitter</a>. Or send me an{' '}
+          <a href="https://www.twitter.com/vnglst"> Twitter</a>. Or send me an{" "}
           <a href="&#x6d;&#x61;&#x69;&#108;&#116;&#111;&#x3a;&#107;&#x6f;&#x65;&#x6e;&#x40;&#x6b;&#x6f;&#x65;&#x6e;&#x76;&#97;&#x6e;&#x67;&#x69;&#108;&#x73;&#116;&#x2e;&#x6e;&#x6c;">
             email
           </a>
@@ -44,7 +44,7 @@ export default () => (
           offline was harder than I thought.
         </p>
         <span className="links">
-          <a href="https://peter-and-wolf.com">Play</a> +{' '}
+          <a href="https://peter-and-the-wolf.netlify.com">Play</a> +{" "}
           <a href="https://github.com/vnglst/peter-and-the-wolf">Source</a> [
           <b>2018</b>]
         </span>
@@ -56,7 +56,7 @@ export default () => (
           I created this fun little PWA for my daughter Nora to help her spell
           her name. She loved it, hope you do too!
         </p>
-        <ul style={{ padding: 0, listStyleType: 'none' }}>
+        <ul style={{ padding: 0, listStyleType: "none" }}>
           <li>
             🚀 <code>create-react-app</code> + TypeScript
           </li>
@@ -64,7 +64,7 @@ export default () => (
           <li>🎺 web audio for sounds</li>
         </ul>
         <span className="links">
-          <a href="https://finding-nora.com">Play</a> +{' '}
+          <a href="https://finding-nora.com">Play</a> +{" "}
           <a href="https://github.com/vnglst/finding-nora">Source</a> [
           <b>2018</b>]
         </span>
@@ -77,9 +77,9 @@ export default () => (
           developer on their loyality app <b>MyTommy</b>.
         </p>
         <span className="links">
-          <Link prefetch href="/hilfiger">
+          <Link href="/hilfiger">
             <a>Demo page (in Dutch)</a>
-          </Link>{' '}
+          </Link>{" "}
           [<b>2018</b>]
         </span>
       </Creations.Item>
@@ -93,9 +93,9 @@ export default () => (
           read for children with dyslexia.
         </p>
         <span className="links">
-          <Link prefetch href="/dedicon">
+          <Link href="/dedicon">
             <a>Demo page (in Dutch)</a>
-          </Link>{' '}
+          </Link>{" "}
           [<b>2017</b>]
         </span>
       </Creations.Item>
@@ -107,10 +107,10 @@ export default () => (
           classic science fiction intro's.
         </p>
         <span className="links">
-          <a href="http://startrek.koenvangilst.nl">Star Trek</a> +{' '}
+          <a href="http://startrek.koenvangilst.nl">Star Trek</a> +{" "}
           <a href="https://github.com/vnglst/startrek ">Source</a>
           <br />
-          <a href="http://starwars.koenvangilst.nl">Star Wars</a> +{' '}
+          <a href="http://starwars.koenvangilst.nl">Star Wars</a> +{" "}
           <a href="https://github.com/vnglst/starwars">Source</a> [<b>2017</b>]
         </span>
       </Creations.Item>
@@ -122,7 +122,7 @@ export default () => (
           translation CAT tool MemoQ. Weekend project using React.
         </p>
         <span className="links">
-          <a href="http://shortcut-trainer.koenvangilst.nl/">Demo</a> +{' '}
+          <a href="http://shortcut-trainer.koenvangilst.nl/">Demo</a> +{" "}
           <a href="https://github.com/vnglst/shortcut-trainer">Source</a> [
           <b>2016</b>]
         </span>
@@ -148,7 +148,7 @@ export default () => (
           the old timelapses.
         </p>
         <span className="links">
-          <a href="https://twitter.com/altijdfiles">@altijdfiles</a> +{' '}
+          <a href="https://twitter.com/altijdfiles">@altijdfiles</a> +{" "}
           <a href="https://github.com/vnglst/spitsuur">Source</a> [<b>2016</b>]
         </span>
       </Creations.Item>
@@ -174,7 +174,7 @@ export default () => (
         </p>
         <p>Technologies used: Express, MongoDB.</p>
         <span className="links">
-          <a href="https://twitter.com/helpTerence"> @helpTerence </a> +{' '}
+          <a href="https://twitter.com/helpTerence"> @helpTerence </a> +{" "}
           <a href="https://github.com/vnglst/HelpTerence">Source</a> [
           <b>2016</b>]
         </span>
@@ -187,7 +187,7 @@ export default () => (
           price per standard line (55 characters).
         </p>
         <span className="links">
-          <a href="http://converter.koenvangilst.nl/">Visit the app</a> +{' '}
+          <a href="http://converter.koenvangilst.nl/">Visit the app</a> +{" "}
           <a href="https://github.com/vnglst/converter2">Source</a> [<b>2016</b>
           ]
         </span>
@@ -196,13 +196,13 @@ export default () => (
       <Creations.Item>
         <h1>Wikipedia edits by the Dutch government</h1>
         <p>
-          Twitter bot inspired by{' '}
+          Twitter bot inspired by{" "}
           <a href="https://twitter.com/congressedits">@congressedits</a> that
           monitors and Tweets any edits made to Wikipedia by the Dutch
           government.
         </p>
         <span className="links">
-          <a href="https://twitter.com/OverheidEdits">@OverheidEdits</a> +{' '}
+          <a href="https://twitter.com/OverheidEdits">@OverheidEdits</a> +{" "}
           <a href="https://github.com/vnglst/OverheidEdits">Source</a> [
           <b>2013</b>]
         </span>
@@ -231,4 +231,4 @@ export default () => (
       </Creations.Item>
     </Creations>
   </Layout>
-)
+);
