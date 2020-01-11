68e80ce27a6622362ae51ad0f92ecd330c109ba9
Koen van Gilst <koen.vangilst@gmail.com>
2020-01-11 20:48:17 +0100

    fix() - adds missing labs folder

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-const withCSS = require('@zeit/next-css')
+const withCSS = require("@zeit/next-css");
 
 module.exports = withCSS({
   exportPathMap: function() {
     return {
-      '/': { page: '/' },
-      '/profile': { page: '/profile' },
-      '/dedicon': { page: '/dedicon' },
-      '/hilfiger': { page: '/hilfiger' }
-    }
+      "/": { page: "/" },
+      "/profile": { page: "/labs" },
+      "/dedicon": { page: "/dedicon" },
+      "/hilfiger": { page: "/hilfiger" }
+    };
   }
-})
+});
