06a83963a48404059eeb3d5678432b7e4a18b03e
Koen van Gilst <koen@koenvangilst.nl>
2020-06-27 15:39:14 +0200

    fix purge tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./components/**/*.js", "./pages/**/*.js"],
+  purge: ["./components/**/*.{js,tsx,ts}", "./pages/**/*.{js,tsx,ts}"],
   theme: {
     extend: {
       colors: {
