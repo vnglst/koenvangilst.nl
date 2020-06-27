0c846a4a9ce247cdaa165bde8b9b2899b46f598c
Koen van Gilst <koen@koenvangilst.nl>
2020-06-27 22:16:53 +0200

    even better footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,15 +2,18 @@ import { FC } from "react";
 
 const Footer: FC = () => {
   return (
-    <footer className="my-10 text-gray-600 text-sm text-center">
+    <footer className="my-10 text-gray-600 text-xs text-center">
       <a href="https://www.linkedin.com/in/vangilst/">LinkedIn</a> |{" "}
       <a href="https://twitter.com/vnglst">Twitter</a> |{" "}
       <a href="https://github.com/vnglst">GitHub</a>
       <br />
-      <a href="https://github.com/vnglst/koenvangilst.nl">source code</a>
-      <br />
-      <span className="text-xs">
-        v. {process.env.APP_VERSION} git {process.env.COMMIT_HASH}
+      <span>
+        v. {process.env.APP_VERSION} git{" "}
+        <a
+          href={`https://github.com/vnglst/koenvangilst.nl/tree/${process.env.COMMIT_HASH}`}
+        >
+          {process.env.COMMIT_HASH}
+        </a>
       </span>
     </footer>
   );
