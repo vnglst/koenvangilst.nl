0ba0f2b583265af4dfa9922522132b57104a7cd9
Koen van Gilst <vnglst@users.noreply.github.com>
2020-06-28 11:27:30 +0200

    chore() - use tailwind + typescript (#52)
    * chore() - refactor css to use tailwind
    
    * fix() - node version
    
    * node version
    
    * switch to npm
    
    * fix npm
    
    * fix navigation
    
    * fix purge tailwindcss
    
    * update screenshot
    
    * add netlify plugin
    
    * remove screenshots
    
    * add footer
    
    * more tyepscript
    
    * last js files
    
    * fix typescript
    
    * typography
    
    * more typography
    
    * more
    
    * even better footer
    
    * better labs
    
    * better font loading
    
    * fix typos
    
    * final check

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ["./components/**/*.{js,tsx,ts}", "./pages/**/*.{js,tsx,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "kvg-orange": "#f06449",
+        "kvg-blue": "#5bc3eb",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
