abda507b917c16116316978718cfd7033cdbc03e
Koen van Gilst <koen@koenvangilst.nl>
2021-11-28 21:06:45 +0100

    cleanup analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,8 @@ import 'styles/global.css';
 
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
-import { useAnalytics } from 'lib/analytics';
 
 export default function App({ Component, pageProps }: AppProps) {
-  useAnalytics();
-
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
