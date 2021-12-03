e1103b24b198a53ef76c45c15e53b38f46d673ff
Koen van Gilst <koen@koenvangilst.nl>
2021-12-03 13:36:38 +0100

    better footer and nav

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -16,7 +16,7 @@ export default function Blog({
           Portfolio
         </h1>
         <p className="mb-4 text-gray-600 dark:text-gray-400">
-          {`I've been a freelance web developer since 2013. I've been involved in all sorts of projects, from a CMS for a narrow casting system, to a mobile app for a global retailer and a web app for investment bankers. In most roles the focus has been on creating the frontend (using React), but I also feel confident creating of contributing to backend services (in TypeScript).`}
+          {`I've been a freelance web developer since 2013. I've been involved in all sorts of projects, from a CMS for a narrow casting system, to a mobile app for a global retailer and a web app for investment bankers. In most roles the focus has been on creating the frontend (using React), but I also feel confident creating and contributing to backend services (in TypeScript).`}
         </p>
         <p className="mb-4 text-gray-600 dark:text-gray-400">
           {`I enjoy working in a multidisciplinary team and I tend to be focused on shipping features that either have improve the user experience or have a postive impact on the organisations bottom line. I'm teamplayer that likes to help other succeed and become a better developer themselves.`}
