7539282806ec999f0601414f0d47d5f59fcd6580
Koen van Gilst <koen@koenvangilst.nl>
2021-12-03 20:57:51 +0100

    fix menu styling

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -45,7 +45,7 @@ export default function MobileMenu() {
         <ul
           className={cn(
             styles.menu,
-            'flex flex-col absolute bg-gray-100 dark:bg-gray-900',
+            'flex flex-col fixed inset-16 bg-gray-100 dark:bg-gray-900',
             isMenuRendered && styles.menuRendered
           )}
         >
