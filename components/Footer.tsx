be9f2dc735312c3afc50d51b96142b9584a9e9e0
Koen van Gilst <koen@koenvangilst.nl>
2021-12-10 09:41:07 +0100

    add version

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,37 +32,25 @@ export default function Footer() {
         <div className="w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 sm:grid-cols-3 mt-8">
           <div className="flex flex-col space-y-4">
             <Link href="/">
-              <a className="text-gray-500 hover:text-gray-600 transition">
-                Home
-              </a>
+              <a className="text-gray-500 hover:text-gray-600">Home</a>
             </Link>
             <Link href="/about">
-              <a className="text-gray-500 hover:text-gray-600 transition">
-                About
-              </a>
+              <a className="text-gray-500 hover:text-gray-600">About</a>
             </Link>
             <Link href="/dashboard">
-              <a className="text-gray-500 hover:text-gray-600 transition">
-                Dashboard
-              </a>
+              <a className="text-gray-500 hover:text-gray-600">Dashboard</a>
             </Link>
           </div>
 
           <div className="flex flex-col space-y-4">
             <Link href="/snippets">
-              <a className="text-gray-500 hover:text-gray-600 transition">
-                Snippets
-              </a>
+              <a className="text-gray-500 hover:text-gray-600">Snippets</a>
             </Link>
             <Link href="/blog/collection-of-great-tweets">
-              <a className="text-gray-500 hover:text-gray-600 transition">
-                Quotes
-              </a>
+              <a className="text-gray-500 hover:text-gray-600">Quotes</a>
             </Link>
             <Link href="/credits">
-              <a className="text-gray-500 hover:text-gray-600 transition">
-                Credits
-              </a>
+              <a className="text-gray-500 hover:text-gray-600">Credits</a>
             </Link>
           </div>
 
@@ -77,6 +65,15 @@ export default function Footer() {
           </div>
         </div>
       </div>
+      <span className="text-gray-500 text-right text-xs my-4">
+        v. {process.env.APP_VERSION} |{' '}
+        <a
+          className="underline"
+          href={`https://github.com/vnglst/koenvangilst.nl/tree/${process.env.COMMIT_HASH}`}
+        >
+          {process.env.COMMIT_HASH}
+        </a>
+      </span>
     </footer>
   );
 }
