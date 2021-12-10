62acc2d872663a8957110e94cab6018011543c68
Koen van Gilst <koen@koenvangilst.nl>
2021-12-10 09:35:27 +0100

    add commit hash to env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 const { withContentlayer } = require('next-contentlayer');
 
+const pkg = require('./package.json');
+
+const commitHash = require('child_process')
+  .execSync('git log --pretty=format:"%h" -n1')
+  .toString()
+  .trim();
+
 /**
  * @type {import('next').NextConfig}
  */
 module.exports = withContentlayer()({
   swcMinify: true,
-  reactStrictMode: true
+  reactStrictMode: true,
+  env: {
+    APP_VERSION: pkg.version,
+    COMMIT_HASH: commitHash
+  }
 });
