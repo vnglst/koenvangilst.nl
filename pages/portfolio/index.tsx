13e022d853b8790fa4d155232c76d259e604119c
Koen van Gilst <koen@koenvangilst.nl>
2021-12-10 15:31:24 +0100

    fix: remove bla bal

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -21,9 +21,6 @@ export default function Blog({
         <p className="mb-4 text-gray-600 dark:text-gray-400">
           {`I've been a freelance web developer since 2013. I've been involved in all sorts of projects, from a CMS for a narrow casting system to a mobile app for a global fashion retailer and a web app for investment bankers. In most roles the focus has been on creating the frontend (using React), but I also feel confident creating and contributing to backend API's (in TypeScript).`}
         </p>
-        <p className="mb-4 text-gray-600 dark:text-gray-400">
-          {`I enjoy working in a multidisciplinary team and I tend to be focused on shipping features either enhancing or improving the user experience. I'm teamplayer that likes to help others and the teamm succeed and become better at their job.`}
-        </p>
         <h3 className="mt-8 mb-8 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
           Recent client projects
         </h3>
