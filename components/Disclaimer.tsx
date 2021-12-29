d042a526cafc98c400f617d50773b2c1f0b827e7
Koen van Gilst <koen@koenvangilst.nl>
2021-12-29 18:50:58 +0100

    add disclaimer to phoenix article

--- components/Disclaimer.tsx
+++ components/Disclaimer.tsx
@@ -1,6 +1,6 @@
 export default function Disclaimer({ children }) {
   return (
-    <div className="bg-gray-100 dark:bg-gray-700 p-2 border-l-8 px-8 border-gray-300 dark:border-gray-500">
+    <div className="bg-gray-200 dark:bg-gray-800 p-2 border-l-8 px-8 border-gray-400 rounded-md dark:border-gray-500">
       {children}
     </div>
   );
