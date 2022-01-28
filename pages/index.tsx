482a5e8eeb7e88515d75bfd1e0f865dfbf0a5315
Koen van Gilst <koen@koenvangilst.nl>
2022-01-28 20:18:02 +0100

    fix: styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,7 +50,7 @@ export default function Home() {
           />
         </section>
         <Link href="/blog">
-          <a className="flex mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6">
+          <a className="flex content-center justify-center justify-items-center item-center mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6">
             Read all posts
             <svg
               xmlns="http://www.w3.org/2000/svg"
