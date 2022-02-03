92d6060bf8a20b6529e1785a26189a7995295e80
Koen van Gilst <koen@koenvangilst.nl>
2022-02-03 21:15:13 +0100

    switching to preact

--- next.config.js
+++ next.config.js
@@ -16,5 +16,17 @@ module.exports = withContentlayer()({
   env: {
     APP_VERSION: pkg.version,
     COMMIT_HASH: commitHash
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat'
+      });
+    }
+
+    return config;
   }
 });
