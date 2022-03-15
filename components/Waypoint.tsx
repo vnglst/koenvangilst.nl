a1efd54669a5e0d523744533c981e35dd70def2a
Koen van Gilst <vnglst@users.noreply.github.com>
2022-03-15 20:18:06 +0100

    add article LiveView cursors (#103)

---
+++ components/Waypoint.tsx
@@ -0,0 +1,10 @@
+export default function Waypoint({ href }) {
+  return (
+    <div className="my-5 md:ml-[-27px]">
+      🏁{' '}
+      <a href={href} className="ml-2">
+        Waypoint
+      </a>
+    </div>
+  );
+}
