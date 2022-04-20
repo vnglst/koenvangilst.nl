6c7707a394f993072035f5c83ad56f02a67d46de
Koen van Gilst <koen@koenvangilst.nl>
2022-04-20 21:14:58 +0200

    fix: dont transition dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import 'styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" disableTransitionOnChange>
       <Component {...pageProps} />
     </ThemeProvider>
   );
