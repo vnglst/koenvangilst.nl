422c5ad105283b1ce364a56813b31d35b213b11c
Koen van Gilst <koen@koenvangilst.nl>
2022-05-27 21:30:17 +0200

    chore: remove dep on unsplash library

--- pages/api/unsplash.ts
+++ pages/api/unsplash.ts
@@ -1,20 +1,22 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import Unsplash, { toJson } from 'unsplash-js';
+import type { NextApiResponse, NextApiRequest } from 'next/types';
 
-let unsplash;
+const ACCESS_TOKEN = process.env.UNSPLASH_ACCESS_KEY;
 
-export default async function handler(
-  req: NextApiRequest,
+export default async function unsplash(
+  _req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (!unsplash) {
-    unsplash = new Unsplash({
-      accessKey: process.env.UNSPLASH_ACCESS_KEY
-    });
-  }
+  const response = await fetch(
+    'https://api.unsplash.com/users/vnglst/statistics',
+    {
+      headers: {
+        Authorization: `Client-ID ${ACCESS_TOKEN}`
+      },
+      method: 'GET'
+    }
+  );
 
-  const userStats = await unsplash.users.statistics('vnglst');
-  const { downloads, views } = await toJson(userStats);
+  const unsplashdata = await response.json();
 
   res.setHeader(
     'Cache-Control',
@@ -22,7 +24,7 @@ export default async function handler(
   );
 
   return res.status(200).json({
-    downloads: downloads.total,
-    views: views.total
+    downloads: unsplashdata.downloads.total,
+    views: unsplashdata.views.total
   });
 }
