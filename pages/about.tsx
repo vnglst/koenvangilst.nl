9b8362298480bfa42bdca73a6743018fd3828967
Koen van Gilst <koen@koenvangilst.nl>
2022-09-01 17:35:36 +0200

    chore: add new job info

--- pages/about.tsx
+++ pages/about.tsx
@@ -23,6 +23,14 @@ export default function About() {
             . Sometimes I&apos;m available for hire for advice, mentoring or
             consulting.
           </p>
+          <p>
+            {`This year I joined Rabobank as a lead frontend developer. In my new
+            role I'll be helping others find their way around the complex
+            environment of the Rabobank. I hope to create a sense of community,
+            so that developers can more easily ask colleagues for help, do code
+            reviews, share code snippets and generally learn from each other.
+            Of course I will still be coding myself as well. I wouldn't want to miss that!`}
+          </p>
         </article>
         <article className="mb-8 prose leading-6 text-gray-600 dark:text-gray-400">
           <h2 className="mt-8 mb-4 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
@@ -62,12 +70,6 @@ export default function About() {
             <li>Minor in Computational Science, Utrecht University.</li>
             <li>Erasmus Student Exchange, Universität Regensburg (Germany).</li>
           </ul>
-        </article>
-        <article className="mb-8 prose leading-6 text-gray-600 dark:text-gray-400">
-          <h2 className="mt-8 mb-4 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
-            References
-          </h2>
-          <p>References, code samples and a copy of my resume upon request.</p>
         </article>
       </article>
     </Container>
