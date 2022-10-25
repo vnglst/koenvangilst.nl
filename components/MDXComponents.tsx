16f384c3b871f3e33d0c92d4beeca2fe174cb7f5
Koen van Gilst <koen@koenvangilst.nl>
2022-10-25 21:36:55 +0200

    chore: upgrade to next 13

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import Image from 'next/future/image';
+import Image from 'next/image';
 
 import Unsplash from './metrics/Unsplash';
 import Disclaimer from './Disclaimer';
