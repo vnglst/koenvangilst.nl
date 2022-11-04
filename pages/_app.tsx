ce2a59e6fe2f0269665539288e36444e0ceed19a
Koen van Gilst <vnglst@users.noreply.github.com>
2022-11-04 20:52:02 +0100

    feature: add Vercel analytics (#117)
    * chore: update deps
    
    * feature: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import 'styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" disableTransitionOnChange>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
