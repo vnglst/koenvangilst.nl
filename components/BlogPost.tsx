391ae2d7807a6bd13bb86375cd35f39f326c0902
Koen van Gilst <koen@koenvangilst.nl>
2023-01-28 19:42:09 +0100

    don't cast number to number

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -11,20 +11,18 @@ export default function BlogPost({
   const { views } = useViews(`/blog/${slug}`);
 
   return (
-    (<Link href={`/blog/${slug}`} className="w-full">
-
+    <Link href={`/blog/${slug}`} className="w-full">
       <div className="w-full mb-8">
         <div className="flex flex-col justify-between md:flex-row">
           <h4 className="w-full mb-2 text-lg font-medium text-gray-900 md:text-xl dark:text-gray-100">
             {title}
           </h4>
-          <p className="w-32 mb-4 text-left text-gray-500 md:text-right md:mb-0">
-            {`${views ? new Number(views).toLocaleString() : '–––'} views`}
+          <p className="w-64 mb-4 text-left text-gray-500 md:text-right md:mb-0">
+            {`${views ? views.toLocaleString() : '–––'} views`}
           </p>
         </div>
         <p className="text-gray-600 dark:text-gray-400">{summary}</p>
       </div>
-
-    </Link>)
+    </Link>
   );
 }
