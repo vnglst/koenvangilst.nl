b97ca19bf984165ecde5ac48f09ee0aeb686d0b4
Koen van Gilst <vnglst@users.noreply.github.com>
2023-04-05 20:44:46 +0200

    add article about enhanced learning (#126)
    * add article about enhanced learning
    
    * add images
    
    * fix: typo
    
    * fix: cleanup next font
    
    * fix: typos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
-import { Inter } from '@next/font/google';
+import { Inter } from "next/font/google";
 
 const interVariable = Inter();
 
