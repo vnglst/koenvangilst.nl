c5cbbd1d40dc9aac153a02b2ab446b832def1ba4
Koen van Gilst <koen@koenvangilst.nl>
2023-04-07 10:47:25 +0200

    fontloading

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,13 +7,6 @@ export default function Document() {
       className="min-w-[360px] scroll-smooth md:overflow-x-scroll"
     >
       <Head>
-        <link
-          rel="preload"
-          href="/fonts/montserrat.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
         <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
         <link href="/static/favicons/site.webmanifest" rel="manifest" />
         <link
