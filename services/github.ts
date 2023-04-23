8a0522ac7af716a6c207956c1d336df9dea11145
Koen van Gilst <koen@koenvangilst.nl>
2023-04-23 12:44:25 +0200

    make sure services are only used on server

--- services/github.ts
+++ services/github.ts
@@ -24,3 +24,9 @@ export async function getGithubStats(): Promise<GitHub> {
     return {};
   }
 }
+
+// `server-only` guarantees any modules that import code in file
+// will never run on the client. Even though this particular api
+// doesn't currently use sensitive environment variables, it's
+// good practise to add `server-only` preemptively.
+import 'server-only';
--- services/unsplash.ts
+++ services/unsplash.ts
@@ -34,3 +34,9 @@ export async function getUnsplashStatistics(): Promise<Unsplash> {
     return {};
   }
 }
+
+// `server-only` guarantees any modules that import code in file
+// will never run on the client. Even though this particular api
+// doesn't currently use sensitive environment variables, it's
+// good practise to add `server-only` preemptively.
+import 'server-only';
