2c63acfdb19c74bce65899527637d5366a4eb742
Koen van Gilst <koen@koenvangilst.nl>
2023-04-23 12:46:36 +0200

    Revert "make sure services are only used on server"
    This reverts commit 8a0522ac7af716a6c207956c1d336df9dea11145.

--- services/unsplash.ts
+++ services/unsplash.ts
@@ -34,9 +34,3 @@ export async function getUnsplashStatistics(): Promise<Unsplash> {
     return {};
   }
 }
-
-// `server-only` guarantees any modules that import code in file
-// will never run on the client. Even though this particular api
-// doesn't currently use sensitive environment variables, it's
-// good practise to add `server-only` preemptively.
-import 'server-only';
