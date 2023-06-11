6a1c1a24ec4b91bc59c1f380cae0c19b98d4062e
Koen van Gilst <koen@koenvangilst.nl>
2023-06-11 14:59:21 +0200

    chore: upgrade to next 13.4.5

--- services/supabase.ts
+++ services/supabase.ts
@@ -3,7 +3,9 @@ const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: { persistSession: false }
+});
 
 type View = {
   created_at: string;
