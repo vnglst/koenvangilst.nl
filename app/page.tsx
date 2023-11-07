77386d0e4cba6d768616ac9cddf684bfa649caf8
Koen van Gilst <koen@koenvangilst.nl>
2023-11-07 18:44:28 +0100

    fix: upgrades

--- app/page.tsx
+++ app/page.tsx
@@ -36,17 +36,14 @@ export default function Home() {
         Featured
       </h3>
       <section className="flex gap-6 flex-col md:flex-row">
-        {/* @ts-expect-error Server Component */}
         <BlogPostCard
           title="Plotting the age of parliament with Livebook"
           slug="livebook-average-age-of-parliament"
         />
-        {/* @ts-expect-error Server Component */}
         <BlogPostCard
           title="Code Colocation is King"
           slug="code-colocation-is-king"
         />
-        {/* @ts-expect-error Server Component */}
         <BlogPostCard
           title="Live user cursors with Phoenix Presence"
           slug="phoenix-live-cursors"
