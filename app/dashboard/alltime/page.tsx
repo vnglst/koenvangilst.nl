c39fce1660d5397341d888fa3c6fe8b7b8467226
Koen van Gilst <koen@koenvangilst.nl>
2023-11-15 19:33:23 +0100

    quicker revalidate

--- app/dashboard/alltime/page.tsx
+++ app/dashboard/alltime/page.tsx
@@ -2,7 +2,7 @@ import ExternalLink from 'components/ExternalLink';
 import VisitsVisual from 'components/VisitsVisual';
 import { getViewsPerDay } from 'services/supabase';
 
-export const revalidate = 60 * 15;
+export const revalidate = 60 * 1;
 
 export const metadata = {
   title: 'All time visits',
