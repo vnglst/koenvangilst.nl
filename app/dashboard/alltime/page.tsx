e7b5c86c755790957e4f181013cc4e8dd799bd8a
Koen van Gilst <koen@koenvangilst.nl>
2023-11-24 19:36:28 +0100

    fix: links in stats page

--- app/dashboard/alltime/page.tsx
+++ app/dashboard/alltime/page.tsx
@@ -24,16 +24,6 @@ export default async function TodaysVisits() {
           <VisualContainer />
         </Suspense>
       </div>
-      <div className="container mx-auto py-8 my-5">
-        <h2 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 my-5">
-          All links with views
-        </h2>
-        <ul className="my-2 min-h-[3500px]">
-          <Suspense>
-            <LinksContainer />
-          </Suspense>
-        </ul>
-      </div>
     </article>
   );
 }
@@ -41,22 +31,4 @@ export default async function TodaysVisits() {
 async function VisualContainer() {
   const visits = await getViewsPerDay(365);
   return <VisitsVisual visits={visits} />;
-}
-
-async function LinksContainer() {
-  const allTime = await getAllTimeList();
-  return allTime.map((item, index) => (
-    <li key={index} className="w-full flex py-3">
-      <Link href={`/blog/${item.url}`} className="w-full">
-        <div className="flex flex-col justify-between md:flex-row up-hover">
-          <h3 className="w-full mb-2 text-lg font-medium text-gray-900 md:text-xl dark:text-gray-100">
-            {item.url}
-          </h3>
-          <span className="w-64 mb-2 text-left text-gray-500 md:text-right md:mb-0">
-            {item.views.toLocaleString()}
-          </span>
-        </div>
-      </Link>
-    </li>
-  ));
 }
