20989f610f73c9effd573e201284e52dc052c1a7
Koen van Gilst <koen@koenvangilst.nl>
2023-12-04 18:33:42 +0100

    refactor: components

--- components/Disclaimer.tsx
+++ components/Disclaimer.tsx
@@ -1,7 +1,1 @@
-export function Disclaimer({ children }) {
-  return (
-    <div className="rounded-md border-l-8 border-gray-400 bg-gray-200 p-2 px-8 dark:border-gray-500 dark:bg-gray-800">
-      {children}
-    </div>
-  );
-}
+export 
