239fb83af57a25c36ce69054f8f16734309b36a9
Koen van Gilst <koen@koenvangilst.nl>
2023-12-08 14:55:53 +0100

    fix: sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,30 +1,42 @@
-import { globby } from 'globby';
+import { getClients, getPosts, getProjects, getSnippets } from 'cms/queries';
 
 export default async function sitemap() {
-  const allPages = await globby([
-    'pages/*.tsx',
-    'data/**/*.mdx',
-    '!data/labs/**/*.mdx',
-    '!data/*.mdx',
-    '!pages/_*.tsx',
-    '!pages/api',
-    '!pages/404.tsx'
-  ]);
+  const allPosts = await getPosts();
+  const allProjects = await getProjects();
+  const allClients = await getClients();
+  const allSnippets = await getSnippets();
 
-  const pages = allPages.map((page) => {
-    const path = page
-      .replace('pages', '')
-      .replace('data', '')
-      .replace('.tsx', '')
-      .replace('.mdx', '');
+  const pages = [
+    'about',
+    'blog',
+    'credits',
+    'dashboard',
+    'dashboard/stats',
+    'labs',
+    'portfolio',
+    'snippets'
+  ];
 
-    const route = path === '/index' ? '' : path;
+  const urls = [
+    ...pages.map((page) => ({
+      url: `https://koenvangilst.nl/${page}`
+    })),
+    ...allPosts.map((post) => ({
+      url: `https://koenvangilst.nl/blog/${post.slug}`
+    })),
+    ...allProjects.map((project) => ({
+      url: `https://koenvangilst.nl/projects/${project.slug}`
+    })),
+    ...allClients.map((client) => ({
+      url: `https://koenvangilst.nl/clients/${client.slug}`
+    })),
+    ...allSnippets.map((snippet) => ({
+      url: `https://koenvangilst.nl/snippets/${snippet.slug}`
+    }))
+  ];
 
-    return {
-      url: `https://koenvangilst.nl${route}`,
-      lastModified: new Date().toISOString()
-    };
-  });
-
-  return pages;
+  return urls.map((url) => ({
+    ...url,
+    lastModified: new Date().toISOString()
+  }));
 }
