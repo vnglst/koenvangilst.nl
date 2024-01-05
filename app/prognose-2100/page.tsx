0b44a0c4e5b146c2dfb5584589c3f755472062d5
Koen van Gilst <koen@koenvangilst.nl>
2024-01-05 08:44:35 +0100

    only display temperature on dashboard

--- app/prognose-2100/page.tsx
+++ app/prognose-2100/page.tsx
@@ -11,8 +11,6 @@ export default async function Prognose2100() {
   return (
     <div className="mb-10 grid w-full grid-cols-1 place-content-center gap-4 lg:grid-cols-2 2xl:grid-cols-3">
       <Anomaly type="temperature" look="dashboard" />
-      <Anomaly type="sunshine" look="dashboard" />
-      <Anomaly type="rain" look="dashboard" />
       <Sealevel />
     </div>
   );
