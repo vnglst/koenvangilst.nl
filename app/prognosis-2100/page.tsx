be6d6c0e37bd07445d1ba355d24bd90ad69446f5
Koen van Gilst <koen@koenvangilst.nl>
2024-01-06 19:00:25 +0100

    rename prognose -> prognosis

--- app/prognosis-2100/page.tsx
+++ app/prognosis-2100/page.tsx
@@ -2,12 +2,12 @@ import { Sealevel } from './(charts)/Sealevel.server';
 import { WeatherAnomaly } from './(charts)/WeatherAnomaly.server';
 
 export const metadata = {
-  title: 'Prognose 2100',
+  title: 'Prognosis 2100',
   description:
-    "Prognose 2100 is a dashboard that allows users to explore the effects of climate change on the world's population."
+    "Prognosis 2100 is a dashboard that allows users to explore the effects of climate change on the world's population."
 };
 
-export default async function Prognose2100() {
+export default async function Prognosis2100() {
   return (
     <div className="mb-10 grid w-full grid-cols-1 place-content-center gap-4 lg:grid-cols-2 2xl:grid-cols-3">
       <WeatherAnomaly type="temperature" look="dashboard" />
