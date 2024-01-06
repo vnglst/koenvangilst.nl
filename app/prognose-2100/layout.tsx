73a7ae996049d5264cd3a19d49a6036d3adc5881
Koen van Gilst <koen@koenvangilst.nl>
2024-01-06 15:38:51 +0100

    sticky toggle

--- app/prognose-2100/layout.tsx
+++ app/prognose-2100/layout.tsx
@@ -6,8 +6,8 @@ import { PrognosisToggle } from './(components)/PrognosisToggle';
 
 export default async function Layout({ children }) {
   return (
-    <div>
-      <nav className="mb-8 flex w-full items-center justify-center gap-5 text-slate-700 dark:text-slate-300">
+    <>
+      <nav className="sticky top-2 z-20 -mx-4 mb-8 flex items-center justify-center gap-4 overflow-hidden bg-white py-1 text-slate-700 md:mx-0 dark:bg-black dark:text-slate-300">
         <Link
           href="/prognose-2100"
           className="bg-primary-bright block whitespace-nowrap rounded-md border-2 border-slate-900 px-3 py-1 text-2xl font-black text-slate-900 dark:border-white dark:text-slate-900"
@@ -15,11 +15,14 @@ export default async function Layout({ children }) {
           Prognosis 2100
         </Link>
         <PrognosisToggle />
-        <Link href="/prognose-2100/about" className="ml-auto">
+        <Link
+          href="/prognose-2100/about"
+          className="ml-auto                 mr-1"
+        >
           <Icon icon="info" className="h-6 w-6" />
         </Link>
       </nav>
       {children}
-    </div>
+    </>
   );
 }
