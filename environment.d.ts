c132277a14a3097fda5ad12c2608eb3c55933c4d
Koen van Gilst <koen@koenvangilst.nl>
2024-01-07 14:47:35 +0100

    add co2 reading office quick & dirty

--- environment.d.ts
+++ environment.d.ts
@@ -2,5 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     SUPABASE_URL: string;
     SUPABASE_ANON_KEY: string;
+    SUPABASE_ARANET_URL: string;
+    SUPABASE_ARANET_ANON_KEY: string;
   }
 }
