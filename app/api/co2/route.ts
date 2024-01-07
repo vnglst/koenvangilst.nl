bfe85b6b3bf1b1df3db69f5ddf2875f0fce750b2
Koen van Gilst <koen@koenvangilst.nl>
2024-01-07 17:51:39 +0100

    use unstable noStore

--- app/api/co2/route.ts
+++ app/api/co2/route.ts
@@ -1,8 +1,12 @@
+import { unstable_noStore } from 'next/cache';
 import { NextResponse } from 'next/server';
 
 import { getLastReading } from 'services/aranet';
 
 export async function GET() {
+  // Disable Next.js response caching
+  // TODO: Do I really need to do this?
+  unstable_noStore();
   const reading = await getLastReading();
   return NextResponse.json(reading);
 }
