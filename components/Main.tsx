0b1bd11e62167aa05929d157c79d4a04acbc35e2
Koen van Gilst <koen@koenvangilst.nl>
2024-03-08 21:45:25 +0100

    update background colors

--- components/Main.tsx
+++ components/Main.tsx
@@ -4,7 +4,7 @@ export function Main({ children }: PropsWithChildren) {
   return (
     <main
       id="content"
-      className="flex flex-1 flex-col break-words bg-gradient-to-b from-white to-slate-50 dark:bg-gray-900 dark:from-black dark:to-slate-900"
+      className="flex flex-1 flex-col break-words bg-gradient-to-b from-white to-[#f6fcfe] dark:bg-gray-900 dark:from-black dark:to-[#02071d]"
     >
       {children}
     </main>
