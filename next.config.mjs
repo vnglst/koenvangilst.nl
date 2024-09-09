235f19c86c4743a4e52f9da6d3a9e3f8aabc6703
Koen van Gilst <koen@koenvangilst.nl>
2024-09-09 09:35:27 +0200

    source commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,18 @@
-import childProcess from 'child_process';
+// import childProcess from 'child_process';
 import fs from 'fs';
 
 import { headers } from './config/next-headers.mjs';
 
 const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+
+// On Vercel, the commit hash can parsed like this
+// const commitHash = childProcess
+//   .execSync('git log --pretty=format:"%h" -n1')
+//   .toString()
+//   .trim();
+
+// On Coolify the commit hash can be found in the environment variable
+const commitHash = process.env.SOURCE_COMMIT;
 
 /**
  * @type {import('next').NextConfig}
@@ -14,7 +23,8 @@ const config = {
   swcMinify: true,
   headers,
   env: {
-    APP_VERSION: pkg.version
+    APP_VERSION: pkg.version,
+    COMMIT_HASH: commitHash
   }
 };
 
