1bbd24c9a634ed7c7f49d7055cabd8f11bb0e28e
Koen van Gilst <koen@koenvangilst.nl>
2024-10-04 18:19:15 +0200

    fix: replace vercel stuff with redirects

---
+++ config/next-redirects.mjs
@@ -0,0 +1,79 @@
+export const redirects = async () => {
+  return [
+    {
+      source: '/react-hooks-with-canvas',
+      destination: '/blog/react-hooks-with-canvas',
+      permanent: true
+    },
+    {
+      source: '/tutorial-svelte-scroll-video',
+      destination: '/blog/tutorial-svelte-scroll-video',
+      permanent: true
+    },
+    {
+      source: '/generating-a-pdf-with-express',
+      destination: '/blog/generating-a-pdf-with-express',
+      permanent: true
+    },
+    {
+      source: '/accessible-svgs',
+      destination: '/blog/accessible-svgs',
+      permanent: true
+    },
+    {
+      source: '/api-testing-with-jest',
+      destination: '/blog/api-testing-with-jest',
+      permanent: true
+    },
+    {
+      source: '/mobx-state-tree',
+      destination: '/blog/mobx-state-tree',
+      permanent: true
+    },
+    {
+      source: '/brutalist-css',
+      destination: '/blog/brutalist-css',
+      permanent: true
+    },
+    {
+      source: '/collection-of-great-tweets',
+      destination: '/blog/collection-of-great-tweets',
+      permanent: true
+    },
+    {
+      source: '/code-colocation-is-king',
+      destination: '/blog/code-colocation-is-king',
+      permanent: true
+    },
+    {
+      source: '/303-see-other',
+      destination: '/blog/303-see-other',
+      permanent: true
+    },
+    {
+      source: '/for-git-me-nots',
+      destination: '/blog/for-git-me-nots',
+      permanent: true
+    },
+    {
+      source: '/dedicon',
+      destination: '/portfolio/dedicon',
+      permanent: true
+    },
+    {
+      source: '/hilfiger',
+      destination: '/portfolio/hilfiger',
+      permanent: true
+    },
+    {
+      source: '/15',
+      destination: 'https://calendly.com/talk-to-koen/15min',
+      permanent: true
+    },
+    {
+      source: '/blog/feed',
+      destination: '/feed.xml',
+      permanent: true
+    }
+  ];
+};
