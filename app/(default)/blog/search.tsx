a29ad434e82c38bbc47e7b69e921e55b602467cc
Koen van Gilst <koen@koenvangilst.nl>
2024-10-24 20:21:34 +0200

    featured instead of most popular posts

--- app/(default)/blog/search.tsx
+++ app/(default)/blog/search.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 
 import { Heading } from 'components/Heading';
-import {Icon} from 'components/Icon';
+import { Icon } from 'components/Icon';
 import { BlogPostLink } from 'components/PostLink';
 
 type Post = {
@@ -49,16 +49,13 @@ export function Search({ posts, placeholderPosts }: SearchProps) {
           placeholder="Search articles"
           className="block w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
         />
-        <Icon
-          icon="search"
-          className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
-        />
+        <Icon icon="search" className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300" />
       </div>
       <div className="flex flex-col gap-6">
         {!query ? (
           <>
             <Heading level={2} className="mb-1">
-              Popular this month
+              Featured
             </Heading>
             {placeholderPosts.map((post) => (
               <BlogPostLink key={post.title} {...post} />
@@ -78,11 +75,7 @@ export function Search({ posts, placeholderPosts }: SearchProps) {
             {searchResults.map((post) => (
               <BlogPostLink key={post.title} {...post} />
             ))}
-            {searchResults.length === 0 && (
-              <p className="mb-4 text-gray-600 dark:text-gray-400">
-                No posts found.
-              </p>
-            )}
+            {searchResults.length === 0 && <p className="mb-4 text-gray-600 dark:text-gray-400">No posts found.</p>}
           </>
         )}
       </div>
