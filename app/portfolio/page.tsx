dd8923f12231df3f972f8c855c24bb48ea204fc0
Koen van Gilst <koen@koenvangilst.nl>
2025-03-19 20:45:16 +0100

    Refine portfolio description and enhance project details

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -8,7 +8,7 @@ import { Prose } from 'components/Prose';
 export const metadata = {
   title: 'Portfolio',
   description:
-    'Experienced Tech Lead at Rabobank with a background in web development and over a decade of experience. Specialized in GenAI, React for frontend design and TypeScript for backend API development.'
+    'Tech Lead at Rabobank with a background in web development and over a decade of experience. Specialized in GenAI, React for frontend design and TypeScript for backend API development.'
 };
 
 export default async function Portfolio() {
@@ -19,17 +19,11 @@ export default async function Portfolio() {
       <Prose>
         <Heading level={1}>Portfolio</Heading>
         <p>
-          I began my career as a web developer, working on diverse projects such as developing a mobile app for a global
-          fashion brand, designing an audio player for users with visual impairments, and engineering a web application
-          for investment banking professionals. My primary focus has been on frontend development, utilizing React and
-          other frameworks to create intuitive user interfaces. Beyond frontend work, I am skilled in backend
-          development with Python, TypeScript and Node.js and enjoy experimenting with Elixir for side projects.
-        </p>
-        <p>
-          In 2022, I transitioned from freelancing to a corporate setting as the Lead Frontend Developer at Rabobank.
-          Building on this experience, I started a new role in 2024 as the Tech Lead GenAI, where I lead the technical
-          development of GenAI-driven solutions. In this role, I oversee technical projects and collaborate with
-          cross-functional teams to advance agrifinance through innovative technology.
+          As Tech Lead GenAI at Rabobank, I lead the technical development of AI-driven solutions, overseeing projects
+          and collaborating with cross-functional teams. My background includes web development experience across
+          diverse projects, from mobile apps for global fashion brands to specialized applications for investment
+          banking. I've focused primarily on frontend development with React & Angular while maintaining skills in
+          backend technologies like Python, TypeScript, and Node.js.
         </p>
       </Prose>
 
