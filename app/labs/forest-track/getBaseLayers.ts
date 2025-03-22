a5f5b3f157f2e188bc45d498421da3121ddd3f9a
Koen van Gilst <vnglst@users.noreply.github.com>
2025-03-22 10:01:51 +0100

    Forest track (#169)

---
+++ app/labs/forest-track/getBaseLayers.ts
@@ -0,0 +1,64 @@
+import { BaseLayer, WaybackConfig } from './types';
+
+type Init = {
+  url: string;
+  yearsBack: number;
+};
+
+/**
+ * This function fetches the base layers from the wayback url and adds the
+ * position property (on a scale of 0 - 100%) to each layer.
+ */
+export async function getBaseLayers(init: Init) {
+  const response = await fetch(init.url);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  const waybackConfig = (await response.json()) as WaybackConfig;
+  return parse(waybackConfig, init.yearsBack);
+}
+
+function parse(waybackConfig: WaybackConfig, yearsBack: number): BaseLayer[] {
+  const layers = Object.entries(waybackConfig).map(([releaseNumber, layer]) => {
+    const date = extractDateFromWaybackItemTitle(layer.itemTitle);
+
+    return {
+      date,
+      releaseNumber: Number(releaseNumber),
+      id: layer.layerIdentifier,
+      url: layer.itemURL,
+      title: layer.itemTitle,
+      position: calcPosition(date, yearsBack)
+    };
+  });
+
+  const sorted = layers.sort((a, b) => a.date.getTime() - b.date.getTime());
+
+  const oldestDateToKeep = new Date();
+  oldestDateToKeep.setFullYear(oldestDateToKeep.getFullYear() - yearsBack);
+
+  return sorted.filter((layer) => layer.date > oldestDateToKeep);
+}
+
+function calcPosition(date: Date, yearsBack: number) {
+  const dateYearsAgo = new Date();
+  dateYearsAgo.setFullYear(dateYearsAgo.getFullYear() - yearsBack);
+  const diff = date.getTime() - dateYearsAgo.getTime();
+  const percentage = diff / (yearsBack * 365 * 24 * 60 * 60 * 1000);
+  return `${Math.round(percentage * 100)}%`;
+}
+
+function extractDateFromWaybackItemTitle(waybackItemTitle = '') {
+  const regexpYYYYMMDD = /\d{4}-\d{2}-\d{2}/g;
+  const results = waybackItemTitle.match(regexpYYYYMMDD);
+  const dateString = results?.length ? results[0] : waybackItemTitle;
+  return convertDateFromWaybackItemTitle(dateString);
+}
+
+function convertDateFromWaybackItemTitle(dateString = '') {
+  const dateParts = dateString.split('-');
+  const year = +dateParts[0];
+  const mon = +dateParts[1] - 1;
+  const day = +dateParts[2];
+  return new Date(year, mon, day);
+}
