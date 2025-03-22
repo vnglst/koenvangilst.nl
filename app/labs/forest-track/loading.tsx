dd41268b6236aa23d330760837d5a45a45f776b4
Koen van Gilst <vnglst@users.noreply.github.com>
2025-03-22 10:01:51 +0100

    Forest track (#169)

---
+++ app/labs/forest-track/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 flex">
+      <div className="m-auto h-16 w-16 animate-spin rounded-full border-b-2 border-t-2 border-gray-900 md:h-32 md:w-32 dark:border-white" />
+    </div>
+  );
+}
---
+++ app/labs/forest-track/useMapSettings.ts
@@ -0,0 +1,76 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useUpdateParams } from 'hooks/useUpdateParams';
+
+import { CONFIG } from './config';
+import { CenterPoint } from './types';
+
+/**
+ * All application state lives in the URL. This hook is responsible for
+ * reading and writing to the URL.
+ */
+export function useMapSettings() {
+  const { searchParams, deleteParam, updateParams } = useUpdateParams();
+
+  const yearsBack = searchParams.get('yearsBack') || CONFIG.yearsBack;
+  const showTreeLoss = searchParams.get('treeloss') !== 'off';
+  const showTreeGain = searchParams.get('treegain') !== 'off';
+  const update = searchParams.get('update') === 'on';
+
+  const centerPoint = useMemo(
+    () => ({
+      latitude: parseFloat(searchParams.get('latitude') || CONFIG.latitude),
+      longitude: parseFloat(searchParams.get('longitude') || CONFIG.longitude),
+      zoom: parseFloat(searchParams.get('zoom') || CONFIG.zoom)
+    }),
+    [searchParams]
+  );
+
+  const [initial, setInitial] = useState({
+    ...centerPoint,
+    showTreeLoss,
+    showTreeGain,
+    yearsBack
+  });
+
+  useEffect(() => {
+    if (!update) return;
+
+    setInitial({
+      ...centerPoint,
+      showTreeLoss,
+      showTreeGain,
+      yearsBack
+    });
+
+    deleteParam('update');
+  }, [centerPoint, deleteParam, showTreeGain, showTreeLoss, update, yearsBack]);
+
+  const handleCenterPointChange = useCallback(
+    (centerPoint: CenterPoint) => {
+      updateParams({
+        latitude: centerPoint.latitude.toString(),
+        longitude: centerPoint.longitude.toString(),
+        zoom: centerPoint.zoom.toString()
+      });
+    },
+    [updateParams]
+  );
+
+  const toggleTreeLossLayer = useCallback(() => {
+    updateParams({ treeloss: showTreeLoss ? 'off' : 'on' });
+  }, [showTreeLoss, updateParams]);
+
+  const toggleTreeGainLayer = useCallback(() => {
+    updateParams({ treegain: showTreeGain ? 'off' : 'on' });
+  }, [showTreeGain, updateParams]);
+
+  return {
+    initial,
+    showTreeLoss,
+    showTreeGain,
+    centerPoint,
+    handleCenterPointChange,
+    toggleTreeLossLayer,
+    toggleTreeGainLayer
+  };
+}
