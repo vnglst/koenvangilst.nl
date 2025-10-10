1a20fbed4bf552c037c698500fd77284a9f18ee8
Koen van Gilst <koen@koenvangilst.nl>
2025-10-10 16:25:25 +0200

    update about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,31 +17,32 @@ export default async function About() {
       <Prose>
         <Heading level={1}>About me</Heading>
         <p>
-          Hi, I'm Koen van Gilst. I've had a diverse career, always operating at the intersection of technology and
-          humanity, with a philosophy degree and a lifelong passion for coding. My journey started with programming at
-          an early age, using <Link href="https://en.wikipedia.org/wiki/GW-BASIC">GW-Basic</Link> and{' '}
-          <Link href="https://en.wikipedia.org/wiki/Turbo_Pascal">Turbo Pascal</Link>. After my studies and working as a
-          translator for a few years, I returned to programming as a frontend engineer, and now to a leadership role
-          where I experiment with new ideas and build innovative teams.
+          Hi, I'm Koen van Gilst. I've had a diverse career, always at the intersection of technology and humanity, with
+          a philosophy degree and a lifelong passion for coding. I started with programming at around 11, using GW-Basic
+          and Turbo Pascal. After my studies and working as a translator for a few years, I returned to programming as a
+          frontend engineer, and now to a technical leadership role where I experiment with new ideas and build
+          innovative teams.
         </p>
         <p>
-          Today, as a Tech Lead at <Link href="https://www.rabobank.nl">Rabobank</Link>, I'm working to build a better
-          bank by reducing complexity and applying AI where it makes sense. I experiment with AI (primarily Large
-          Language Models) and design technical tests to validate potential solutions. My role involves building and
-          supporting teams, recruiting talent, and securing resources to drive projects forward. I collaborate closely
-          with business stakeholders and engineers to align our strategies with product goals, guiding projects from
-          initial experimentation to successful MVPs.
+          At <Link href="https://www.rabobank.nl">Rabobank</Link> I'm focused on building a better bank by applying AI
+          where it makes sense. I experiment with Large Language Models and design technical prototypes to validate
+          solutions. My main goal is always to make users happy and GenAI offers a unique opportunity to make their
+          daily working lives easier and more fun. To achieve this I collaborate closely with business stakeholders and
+          engineers to align our strategies with product goals, guiding projects from initial experimentation to
+          successful products in the hands of our users.
         </p>
         <p>
-          At the core of my work is a commitment to stimulating an engineering culture that values craftsmanship,
-          technical expertise, and continuous learning. I strive to create environments where teams are empowered to use
-          the right technological solutions, enabling them to excel and help the Rabobank innovate with AI.
+          Central to my work is stimulating an engineering culture that values craftsmanship, technical expertise, and
+          continuous learning. I feel strongly that many of today's innovations (like GenAI) are technology driven,
+          which means that technical experts need to be there when solutions are proposed and discussed. That's why I
+          strive to ensure technical teams are empowered to use the right technological solutions, enabling them to help
+          the Rabobank innovate with AI.
         </p>
         <p>
-          My work aligns perfectly with my passion for technology. In my spare time, I enjoy coding side projects that
-          often complement my professional work - a great way to stay current with trends and keep learning (though it
-          can be a double-edged sword). On this website, you'll find a selection of my{' '}
-          <Link href="/lab?q=side-project">side projects</Link> and other digital creations from over the years.
+          My work aligns with my passion for technology. In my spare time, I enjoy coding side projects that often
+          complement my professional work - a great way to stay up to date with trends and keep learning. On this
+          website, you'll find a selection of my <Link href="/lab?q=side-project">side projects</Link> and other digital
+          creations from over the years.
         </p>
       </Prose>
     </Container>
