40c69aa769421e11845480d42727c8ecfde37254
Koen van Gilst <koen@koenvangilst.nl>
2025-11-24 21:25:21 +0100

    fix max width

--- components/layout/Main.tsx
+++ components/layout/Main.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 export function Main({ children }: PropsWithChildren) {
   return (
-    <main id="content" className="flex flex-1 flex-col break-words">
+    <main id="content" className="flex w-full flex-1 flex-col break-words">
       {children}
     </main>
   );
