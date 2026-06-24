5de2a21a63561a3089eed7c3ec6173ae2ade48ac
Koen van Gilst <koen@koenvangilst.nl>
2026-06-24 15:46:47 +0200

    cleanup redirects

--- src/routes/about.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/about')({
-  beforeLoad: () => {
-    throw redirect({ to: '/', statusCode: 301 });
-  }
-});
